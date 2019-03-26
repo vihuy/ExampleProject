abf0c99579d28fd2316b6f49721e48a54353faf6
Vi Tran <vitran@Vis-MacBook-Pro.local>
2019-03-26 14:35:49 +0700

    add code

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,7 @@
+class ProductsController < ApplicationController
+  def index
+  end
+  def create
+    byebug
+  end
+end
---
+++ db/migrate/20190322043400_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.float :price
+      t.references :category, index: true
+      t.timestamps
+    end
+  end
+end
